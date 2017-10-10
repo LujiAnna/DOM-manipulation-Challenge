98fa9cb264d97eb736bb4adc1c2e15d82a144e8d
Rachael <rachaelcodes@users.noreply.github.com>
2017-10-10 10:17:49 +0100

    updated solutions for shorter exercise
    now the exercise is shorter, we don't need so many solutions...

--- exercise/exercise.js
+++ exercise/exercise.js
@@ -7,21 +7,6 @@ might be generalised to be more useful.
 /* EXERCISES */
 
 /*
-Write a function which adds a class "highlight" to the introduction paragraph
-*/
-var highlightText = function(){
-  document.querySelector(".introduction").classList.add("highlight");
-};
-
-/*
-Write a function which removes the class "highlight" from the introduction
-paragraph
-*/
-var unHighlightIntroText = function(){
- document.querySelector(".introduction").classList.remove("highlight");
-};
-
-/*
 Write a function which, if the introduction paragraph has the class "highlight",
 it will be removed; otherwise the class "highlight" is added.
 */
@@ -34,43 +19,6 @@ Write a function which returns all text content of the introduction paragraph
 */
 var getText = function(){
   return document.querySelector(".introduction").innerText;
-}
-
-/*
-Write a function which returns the number of charachters in the introduction
-paragraph
-*/
-var getNumChars = function(){
-  return getText().length;
-}
-
-/*
-Write a function which returns the number of words in the introduction paragraph
-*/
-var getNumWords = function(){
-  return getText().split(" ").length;
-}
-
-/*
-Write a function which returns the number of sentences in the introduction
-paragraph
-*/
-var getNumSentences = function(){
-  return getText().split(".").length;
-}
-
-/*
-Write a function which returns the average number of words per sentence
-*/
-var getWordsPerSentece = function(){
-  return getNumSentences()/getNumWords();
-}
-
-/*
-Write a function which returns the average number of letters per word
-*/
-var getLettersPerWord = function(){
-  return getNumChars()/getNumWords();
 }
 
 /*
@@ -138,15 +86,6 @@ var getInputValues(className){
 };
 
 /*
-Write a function which takes a className, and returns the number of elements in
-the DOM with that className. If the function is passed an argument which doesn't
-have type `string`, the function should return 0;
-*/
-var getNumElsOfClass = function(className){
-  return document.querySelectorAll(className).length;
-};
-
-/*
 Write a function, `generateUl`, which takes an array of strings, and returns a
 `ul` htmlElement containing `li` elements for each array element. Each `li'
 should contain the value of the array element.
@@ -164,20 +103,3 @@ var generateUl = function(arr){
   });
   return ulNode;
 };
-
-/*
-Write a function, `generateNestedUl`, which takes array whose elements can be
-either strings or arrays of strings, and which returns a `ul` htmlElement
-containing `li` elements for each array element. Each `li` should contain the
-value of the array element if is a string, or a `ul` containing `li` elements
-of the array if element is an array.
-*/
-var generateNestedUl = function(array) {
-
-}
-
-/*
-Using the above functions, write a function which takes a filter function,
-which returns a function that takes an array, that generates a `ul` as above
-but with filtered elements.
-*/
